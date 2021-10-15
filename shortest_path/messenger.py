fcde91e93ee6b78d16e6597bef62b52ed207fb85
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-10-15 15:34:00 +0900

    🪱 최단 경로 구하기

---
+++ shortest_path/messenger.py
@@ -0,0 +1,48 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, c = map(int, input().split())
+
+graph = [[] for i in range(n + 1)]
+
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(c)
+
+city_num = 0
+max_time = 0
+
+
+for d in distance:
+    if d != INF:
+        city_num += 1
+        max_time = max(max_time, d)
+
+
+print(city_num-1, max_time)
