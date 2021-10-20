61cf1fd40e3835b47b413b8e2c2da1e25744c947
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-10-20 17:03:55 +0900

    😊 계수 정렬 소스 코드

---
+++ sorting/count_sort.py
@@ -0,0 +1,13 @@
+# 모든 범위를 담을 수 있는 크기의 리스트(배열)를 선언해야 함
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end=" ")
+
